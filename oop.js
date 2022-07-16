481fc828c2e46f3702c4aa66ceab70ee2e481f03
Spencer Haka <spencer.haka@gmail.com>
2022-07-16 15:39:01 -0600

    complete all assigned work and self-eval

--- oop.js
+++ oop.js
@@ -2,7 +2,18 @@
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class unicorn {
+  constructor(name) {
+    this.name = name;
+    this.horn = true;
+    this.color = silver;
+    this.home = "Rainbow Land";
+  }
 
+  say(string) {
+    console.log(`*~*${string}*~*`)
+  }
+}
 
 
 // Write a class called Vampire
@@ -11,6 +22,18 @@
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
 
+class vampire {
+  constructor(name, pet = "bat") {
+    this.name = name;
+    this.pet = pet;
+    this.thirsty = true;
+  }
+
+  drink(){
+    this.thirsty = false;
+    console.log(`I no longer vant to suck your blooood!`);
+  }
+}
 
 
 //  Write a Dragon class
@@ -20,7 +43,20 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+class dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.isHungry = true;
+  }
 
+  eat(numberOfTimes) {
+    if (numberOfTimes > 4) {
+      this.isHungry = false;
+    }
+  }
+}
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +67,39 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class hobbit {
+  constructor(name, disposition) {
+    this.name = name;
+    this.disposition = disposition;
+    this.age = 0;
+    this.isAdult = false;
+    this.isOld = false;
+    this.hasRing = false;
+    if (this.name === "frodo"){
+      this.hasRing = "true"
+    }
+  }
+
+  celebrateBirthday(times) {
+    this.age += times;
+    if (this.age > 32) {
+      this.isAdult = true;
+    }
+    if (this.age > 100) {
+      this.isOld = true;
+    }
+  }
+}
+
+let hobbit1 = new hobbit("frodo", "hesitant");
+let hobbit2 = new hobbit("samwise", "brave");
+
+console.log(hobbit1);
+console.log(hobbit2);
+
+hobbit1.celebrateBirthday(34);
+hobbit1.celebrateBirthday(80);
+
+console.log(hobbit1);
+console.log(hobbit2);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+  This was certainly a challenge. I've never worked with Javascript Classes before. But considering how much we've already learned I knew I would be able to figure it out and leaned on a lot of the information we had already learned through mod0.
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+  I think it's going well. I have my set times of the day when I try to get things done. That ebbs and flows since not every day works out exactly as planned. But so far so good and I think this routine is going to work going into Mod 1.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+  I've got most things ironed out in regards to my schedule, routine, home, family, and technical aspects. I just have a few housekeeping items to attend to.
